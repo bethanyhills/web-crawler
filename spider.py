263da7eab028beb7ff77e529655d2a8c5db04ad2
bethanyhills <beth0851@gmail.com>
2016-10-03 22:22:27 -0500

    add comments and todos

--- spider.py
+++ spider.py
@@ -2,27 +2,47 @@ import sqlite3
 import urllib.request
 from bs4 import BeautifulSoup
 
-'''Web crawler that searches for a key word and if found adds the URL to the database'''
+''' Web crawler that searches for a key word within the pages it crawls.
+    if keyword is found it updates the entry in the database.
+'''
 
 conn = sqlite3.connect('db.sqlite')
 cur = conn.cursor()
 
-def fetchHTML(url):
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        return html
+class pageMagic(object):
+    def __init__(self, url):
+        self.url = url
+
+    def fetchHTML(self):
+        with urllib.request.urlopen(self.url) as response:
+            self.html = response.read()
+            return self.html
+
+    def keywordSearch(self, keyword):
+        self.soup = BeautifulSoup(self.html, 'html.parser')
+        if keyword in self.soup.get_text():
+            print (keyword + ' found!')
+            self.keyword = True
+            return self.keyword
+            #TODO: update db entry with keyword search results
+
+    def getLinks(self):
+        links = []
+        for item in self.soup.find_all('a'):
+            link = item.get('href')
+            if not link:
+                continue
+            else:
+                links.append(link)
+        # TODO: write to db
+        #print (links)
+
+def spider(start_url, keyword, max_urls):
+    #TODO: start crawl, continue with next entry in the db
+    page = pageMagic(start_url)
+    page.fetchHTML()
+    page.keywordSearch(keyword)
+    page.getLinks()
 
 
-def keywordSearch(html, keyword):
-    soup = BeautifulSoup(html, 'html.parser')
-    if keyword in soup.get_text():
-        print (keyword + ' found!')
-        return True
-
-page = fetchHTML("http://isthelakefullyet.com/")
-keywordSearch(page, 'Lake')
-
-
-# def spider(url, word):
-#     soup = BeautifulSoup()
-#     #search page
+spider("http://isthelakefullyet.com/", "Lake", 10)
