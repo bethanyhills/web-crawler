bbb637712e32b2033cf53b3a9aa54514c1cd5f29
bethanyhills <beth0851@gmail.com>
2016-10-03 21:41:56 -0500

    added db connection and keyword search function

--- spider.py
+++ spider.py
@@ -4,12 +4,23 @@ from bs4 import BeautifulSoup
 
 '''Web crawler that searches for a key word and if found adds the URL to the database'''
 
+conn = sqlite3.connect('db.sqlite')
+cur = conn.cursor()
+
 def fetchHTML(url):
     with urllib.request.urlopen(url) as response:
         html = response.read()
-        print (html)
+        return html
 
-fetchHTML("http://isthelakefullyet.com/")
+
+def keywordSearch(html, keyword):
+    soup = BeautifulSoup(html, 'html.parser')
+    if keyword in soup.get_text():
+        print (keyword + ' found!')
+        return True
+
+page = fetchHTML("http://isthelakefullyet.com/")
+keywordSearch(page, 'Lake')
 
 
 # def spider(url, word):
